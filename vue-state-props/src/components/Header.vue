98af7c1947eb92253fbd7cbe068ee213c15f3885
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2022-04-26 11:10:51 +0200

    Added state_props

---
+++ vue-state-props/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>Antal click: {{ numberOfClicks }}</p>
+  <div>
+    <h4>{{ todo.text }}</h4>
+    <p>Klar: {{ todo.done }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Todo } from "@/models/Todo";
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    todo: Todo,
+  },
+})
+export default class Header extends Vue {
+  todo!: Todo;
+  numberOfClicks = 0;
+
+  mounted() {
+    console.log("Start", this.todo);
+    this.numberOfClicks++;
+  }
+}
+</script>
+
+<style lang="scss"></style>
---
+++ vue-state-props/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+  <div>
+    <Header :todo="shop"></Header>
+  </div>
+</template>
+
+<script lang="ts">
+import { Todo } from "@/models/Todo";
+import { Options, Vue } from "vue-class-component";
+import Header from "./Header.vue";
+
+@Options({
+  props: {
+    msg: String,
+  },
+  components: {
+    Header,
+  },
+})
+export default class HelloWorld extends Vue {
+  msg!: string;
+  shop: Todo = new Todo("Handla");
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
